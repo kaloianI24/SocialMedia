1cf70e6f4a155f2dbbdcb902aa3797023e04e9b3
D3a7hKn1gh7 <angelpisarev@gmail.com>
2025-02-02 17:46:04 +0200

    IGenericService
    will do more services in the future, have stop and do something with my family

--- Service/SocialMedia.Service/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SocialMedia.Service
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Service/SocialMedia.Service/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿namespace SocialMedia.Service
+{
+    public interface IGenericService<TEntity, TModel>
+    {
+        IQueryable<TModel> GetAll();
+
+        TModel GetByIdAsync(string id);
+
+        TModel CreateAsync(TModel model);
+        
+        TModel UpdateAsync(TModel model);
+
+        TModel DeleteAsync(string id);
+    }
+}
