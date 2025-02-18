4650139e304fdabc47cf3bebb05dc2cd84e9ab7f
D3a7hKn1gh7 <angelpisarev@gmail.com>
2025-02-18 21:53:01 +0200

    half finished reaction
    idk what the actual f is going on and idk why some of the reactions are like a christmas tree :D

---
+++ Web/SocialMedia.Web.Models/Reaction/CreateReactionModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Web.Models.Reaction
+{
+    public class CreateReactionModel
+    {
+        public string Label { get; set; }
+
+        public IFormFile Emote { get; set; }
+
+    }
+}
