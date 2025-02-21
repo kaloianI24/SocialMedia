6ac01418d0b0cc49a9d30b0692069d104bfcf923
D3a7hKn1gh7 <angelpisarev@gmail.com>
2025-02-21 23:23:18 +0200

    basic comment logic
    getting late, need sleep, this project gonna be my 13th reason

---
+++ Service/SocialMedia.Service.Models/SocialMediaCommentServiceModel.cs
@@ -0,0 +1,20 @@
+﻿using SocialMedia.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Service.Models
+{
+    public class SocialMediaCommentServiceModel : MetadataBaseServiceModel
+    {
+        public string Content { get; set; }
+
+        public List<CloudResourceServiceModel> Attachments { get; set; }
+
+        public List<UserCommentReactionServiceModel> Reactions { get; set; }
+
+        public List<SocialMediaCommentServiceModel> Replies { get; set; }
+    }
+}
